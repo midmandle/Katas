29f57ef5b63cca4c8544b08dd5f5cf47192cfb6f
Alasdair Smith <alasdairsmith0491@gmail.com>
2018-09-30 14:39:33 +0100

    Renamed the constant variables to be more visually distinguishable.

--- KataBankOCR/lib/test/ocr.spec.js
+++ KataBankOCR/lib/test/ocr.spec.js
@@ -5,52 +5,52 @@ var Ocr = require('../ocr');
 
 describe('OCR', function() {
 
-    const zeroText = 
+    const ZERO_TEXT = 
         ' _ '+
         '| |'+
         '|_|';
 
-    const oneText = 
+    const ONE_TEXT = 
         '   '+
         ' | '+
         ' | ';
     
-    const twoText = 
+    const TWO_TEXT = 
         ' _ '+
         ' _|'+
         '|_ ';
 
-    const threeText = 
+    const THREE_TEXT = 
         ' _ '+
         ' _|'+
         ' _|';
 
-    const fourText = 
+    const FOUR_TEXT = 
         '   '+
         '|_|'+
         '  |';
     
-    const fiveText = 
+    const FIVE_TEXT = 
         ' _ '+
         '|_ '+
         ' _|';
     
-    const sixText = 
+    const SIX_TEXT = 
         ' _ '+
         '|_ '+
         '|_|';
 
-    const sevenText = 
+    const SEVEN_TEXT = 
         ' _ '+
         '  |'+
         '  |';
     
-    const eightText = 
+    const EIGHT_TEXT = 
         ' _ '+
         '|_|'+
         '|_|';
 
-    const nineText = 
+    const NINE_TEXT = 
         ' _ '+
         '|_|'+
         '  |';
@@ -58,61 +58,61 @@ describe('OCR', function() {
     it('processChar() should return 0 if given a text zero.', function() {
         const ocrProcessor = new Ocr();
 
-        expect(ocrProcessor.mapEntryCharToValue(zeroText)).to.equal(0);
+        expect(ocrProcessor.mapEntryCharToValue(ZERO_TEXT)).to.equal(0);
     });
 
     it('processChar() should return 1 if given a text one.', function() {
         const ocrProcessor = new Ocr();
 
-        expect(ocrProcessor.mapEntryCharToValue(oneText)).to.equal(1);
+        expect(ocrProcessor.mapEntryCharToValue(ONE_TEXT)).to.equal(1);
     });
 
     it('processChar() should return 2 if given a text two.', function() {
         const ocrProcessor = new Ocr();
 
-        expect(ocrProcessor.mapEntryCharToValue(twoText)).to.equal(2);
+        expect(ocrProcessor.mapEntryCharToValue(TWO_TEXT)).to.equal(2);
     });
 
     it('processChar() should return 3 if given a text three.', function() {
         const ocrProcessor = new Ocr();
 
-        expect(ocrProcessor.mapEntryCharToValue(threeText)).to.equal(3);
+        expect(ocrProcessor.mapEntryCharToValue(THREE_TEXT)).to.equal(3);
     });
 
     it('processChar() should return 4 if given a text four.', function() {
         const ocrProcessor = new Ocr();
 
-        expect(ocrProcessor.mapEntryCharToValue(fourText)).to.equal(4);
+        expect(ocrProcessor.mapEntryCharToValue(FOUR_TEXT)).to.equal(4);
     });
 
     it('processChar() should return 5 if given a text five.', function() {
         const ocrProcessor = new Ocr();
 
-        expect(ocrProcessor.mapEntryCharToValue(fiveText)).to.equal(5);
+        expect(ocrProcessor.mapEntryCharToValue(FIVE_TEXT)).to.equal(5);
     });
 
     it('processChar() should return 6 if given a text six.', function() {
         const ocrProcessor = new Ocr();
 
-        expect(ocrProcessor.mapEntryCharToValue(sixText)).to.equal(6);
+        expect(ocrProcessor.mapEntryCharToValue(SIX_TEXT)).to.equal(6);
     });
 
     it('processChar() should return 7 if given a text seven.', function() {
         const ocrProcessor = new Ocr();
 
-        expect(ocrProcessor.mapEntryCharToValue(sevenText)).to.equal(7);
+        expect(ocrProcessor.mapEntryCharToValue(SEVEN_TEXT)).to.equal(7);
     });
 
     it('processChar() should return 8 if given a text eight.', function() {
         const ocrProcessor = new Ocr();
 
-        expect(ocrProcessor.mapEntryCharToValue(eightText)).to.equal(8);
+        expect(ocrProcessor.mapEntryCharToValue(EIGHT_TEXT)).to.equal(8);
     });
 
     it('processChar() should return 9 if given a text nine.', function() {
         const ocrProcessor = new Ocr();
 
-        expect(ocrProcessor.mapEntryCharToValue(nineText)).to.equal(9);
+        expect(ocrProcessor.mapEntryCharToValue(NINE_TEXT)).to.equal(9);
     });
 
     it('processChar() should throw an ILL error if given an unrecognizable char.', function() {
@@ -146,15 +146,15 @@ describe('OCR', function() {
             expect(/^[_| ]*$/.test(extractedChar)).to.be.true;
         });
 
-        expect(returnValue[0]).to.be.equal(oneText);
-        expect(returnValue[1]).to.be.equal(twoText);
-        expect(returnValue[2]).to.be.equal(threeText);
-        expect(returnValue[3]).to.be.equal(fourText);
-        expect(returnValue[4]).to.be.equal(fiveText);
-        expect(returnValue[5]).to.be.equal(sixText);
-        expect(returnValue[6]).to.be.equal(sevenText);
-        expect(returnValue[7]).to.be.equal(eightText);
-        expect(returnValue[8]).to.be.equal(nineText);       
+        expect(returnValue[0]).to.be.equal(ONE_TEXT);
+        expect(returnValue[1]).to.be.equal(TWO_TEXT);
+        expect(returnValue[2]).to.be.equal(THREE_TEXT);
+        expect(returnValue[3]).to.be.equal(FOUR_TEXT);
+        expect(returnValue[4]).to.be.equal(FIVE_TEXT);
+        expect(returnValue[5]).to.be.equal(SIX_TEXT);
+        expect(returnValue[6]).to.be.equal(SEVEN_TEXT);
+        expect(returnValue[7]).to.be.equal(EIGHT_TEXT);
+        expect(returnValue[8]).to.be.equal(NINE_TEXT);       
     });
 
     it('processEntry() should return the numeric representation of the entry text.', function() {
@@ -174,6 +174,6 @@ describe('OCR', function() {
 
     it('convertTextNumbersToEntryValue() should take an array of text numbers and return a number.', function() {
         const ocrProcessor = new Ocr();
-        expect(ocrProcessor.convertTextNumbersToEntryValue([oneText, twoText, threeText, fourText, fiveText, sixText, sevenText, eightText, nineText])).to.equal(123456789);
+        expect(ocrProcessor.convertTextNumbersToEntryValue([ONE_TEXT, TWO_TEXT, THREE_TEXT, FOUR_TEXT, FIVE_TEXT, SIX_TEXT, SEVEN_TEXT, EIGHT_TEXT, NINE_TEXT])).to.equal(123456789);
     });
 });
