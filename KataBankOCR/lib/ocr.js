13e763f1dd6c555aaa3bf865ef503b19c089709c
Alasdair Smith <alasdairsmith0491@gmail.com>
2018-09-30 14:36:33 +0100

    Added more functionality to the OCR class.

--- KataBankOCR/lib/ocr.js
+++ KataBankOCR/lib/ocr.js
@@ -1,78 +1,117 @@
 class Ocr {
 
-    constructor() {
-        this.zeroChar = 
+    constructor(lineLength) {
+        this.LINE_LENGTH = lineLength;
+        this.ZERO_CHAR = 
             ' _ '+
             '| |'+
             '|_|';
 
-        this.oneChar = 
+        this.ONE_CHAR = 
             '   '+
             ' | '+
             ' | ';
 
-        this.twoChar = 
+        this.TWO_CHAR = 
             ' _ '+
             ' _|'+
             '|_ ';
 
-        this.threeChar = 
+        this.THREE_CHAR = 
             ' _ '+
             ' _|'+
             ' _|';
         
-        this.fourChar = 
+        this.FOUR_CHAR = 
             '   '+
             '|_|'+
             '  |';
         
-        this.fiveChar = 
+        this.FIVE_CHAR = 
             ' _ '+
-            '|_'+
+            '|_ '+
             ' _|';
         
-        this.sixChar = 
+        this.SIX_CHAR = 
             ' _ ' + 
             '|_ ' +
             '|_|';
         
-        this.sevenChar = 
+        this.SEVEN_CHAR = 
             ' _ '+
             '  |'+
             '  |';
         
-        this.eightChar = 
+        this.EIGHT_CHAR = 
             ' _ '+
             '|_|'+
             '|_|';
         
-        this.nineChar = 
+        this.NINE_CHAR = 
             ' _ '+
             '|_|'+
             '  |';
     }
 
-    processChar(numberChar) {
+    processEntry(threeLineEntry) {
+        let individualNumbersAsText = this.extractIndividualNumbers(threeLineEntry);
+        let entryAsNumericValue = this.convertTextNumbersToEntryValue(individualNumbersAsText);
+        return entryAsNumericValue;
+    }
+
+    extractIndividualNumbers(threeLineEntry) {
+        const lineLength = 27;
+        //get top of chars.
+        const top = threeLineEntry.slice(0, lineLength);
+        //get middle of chars.
+        const middle = threeLineEntry.slice(lineLength, lineLength*2);
+        //get bottom of char.
+        const bottom = threeLineEntry.slice(lineLength*2, lineLength*3);
+
+        let individualNumbers = [];
+
+        for (let i = 0; i < 9; i++) {
+                let startIndex = i * 3;
+                let endIndex = startIndex + 3;
+                individualNumbers[i] = top.slice(startIndex, endIndex);
+                individualNumbers[i] += middle.slice(startIndex, endIndex);
+                individualNumbers[i] += bottom.slice(startIndex, endIndex);
+        }
+
+        return individualNumbers;
+    }
+
+    convertTextNumbersToEntryValue(individualNumbersAsText) {
+        let individualNumericValue = individualNumbersAsText.map((stringValue) => {
+            return this.mapEntryCharToValue(stringValue);
+        });
+
+        let entryAsStringOfNumbers = individualNumericValue.join('');
+        let entryAsNumericValue = Number.parseInt(entryAsStringOfNumbers);
+        return entryAsNumericValue;
+    }
+
+    mapEntryCharToValue(numberChar) {
         switch(numberChar) {
-            case(this.zeroChar) :
+            case(this.ZERO_CHAR) :
                 return 0;
-            case(this.oneChar) :
+            case(this.ONE_CHAR) :
                 return 1;
-            case(this.twoChar) :
+            case(this.TWO_CHAR) :
                 return 2;
-            case(this.threeChar) :
+            case(this.THREE_CHAR) :
                 return 3;
-            case(this.fourChar) :
+            case(this.FOUR_CHAR) :
                 return 4;
-            case(this.fiveChar) :
+            case(this.FIVE_CHAR) :
                 return 5;
-            case(this.sixChar) :
+            case(this.SIX_CHAR) :
                 return 6;
-            case(this.sevenChar) :
+            case(this.SEVEN_CHAR) :
                 return 7;
-            case(this.eightChar) :
+            case(this.EIGHT_CHAR) :
                 return 8;
-            case(this.nineChar) :
+            case(this.NINE_CHAR) :
                 return 9;
             default:
                 throw new Error('ILL');
--- KataBankOCR/lib/test/ocr.spec.js
+++ KataBankOCR/lib/test/ocr.spec.js
@@ -4,114 +4,115 @@ var expect = chai.expect;
 var Ocr = require('../ocr');
 
 describe('OCR', function() {
-    it('processChar() should return 0 if given a text zero.', function() {
-        const ocrProcessor = new Ocr();
 
-        const zero = 
+    const zeroText = 
         ' _ '+
         '| |'+
         '|_|';
 
-        expect(ocrProcessor.processChar(zero)).to.equal(0);
+    const oneText = 
+        '   '+
+        ' | '+
+        ' | ';
+    
+    const twoText = 
+        ' _ '+
+        ' _|'+
+        '|_ ';
+
+    const threeText = 
+        ' _ '+
+        ' _|'+
+        ' _|';
+
+    const fourText = 
+        '   '+
+        '|_|'+
+        '  |';
+    
+    const fiveText = 
+        ' _ '+
+        '|_ '+
+        ' _|';
+    
+    const sixText = 
+        ' _ '+
+        '|_ '+
+        '|_|';
+
+    const sevenText = 
+        ' _ '+
+        '  |'+
+        '  |';
+    
+    const eightText = 
+        ' _ '+
+        '|_|'+
+        '|_|';
+
+    const nineText = 
+        ' _ '+
+        '|_|'+
+        '  |';
+    
+    it('processChar() should return 0 if given a text zero.', function() {
+        const ocrProcessor = new Ocr();
+
+        expect(ocrProcessor.mapEntryCharToValue(zeroText)).to.equal(0);
     });
 
     it('processChar() should return 1 if given a text one.', function() {
         const ocrProcessor = new Ocr();
 
-        const one = 
-        '   '+
-        ' | '+
-        ' | ';
-
-        expect(ocrProcessor.processChar(one)).to.equal(1);
+        expect(ocrProcessor.mapEntryCharToValue(oneText)).to.equal(1);
     });
 
     it('processChar() should return 2 if given a text two.', function() {
         const ocrProcessor = new Ocr();
 
-        const two = 
-        ' _ '+
-        ' _|'+
-        '|_ ';
-
-        expect(ocrProcessor.processChar(two)).to.equal(2);
+        expect(ocrProcessor.mapEntryCharToValue(twoText)).to.equal(2);
     });
 
     it('processChar() should return 3 if given a text three.', function() {
         const ocrProcessor = new Ocr();
 
-        const three = 
-        ' _ '+
-        ' _|'+
-        ' _|';
-
-        expect(ocrProcessor.processChar(three)).to.equal(3);
+        expect(ocrProcessor.mapEntryCharToValue(threeText)).to.equal(3);
     });
 
     it('processChar() should return 4 if given a text four.', function() {
         const ocrProcessor = new Ocr();
 
-        const four = 
-        '   '+
-        '|_|'+
-        '  |';
-
-        expect(ocrProcessor.processChar(four)).to.equal(4);
+        expect(ocrProcessor.mapEntryCharToValue(fourText)).to.equal(4);
     });
 
     it('processChar() should return 5 if given a text five.', function() {
         const ocrProcessor = new Ocr();
 
-        const five = 
-        ' _ '+
-        '|_'+
-        ' _|';
-
-        expect(ocrProcessor.processChar(five)).to.equal(5);
+        expect(ocrProcessor.mapEntryCharToValue(fiveText)).to.equal(5);
     });
 
     it('processChar() should return 6 if given a text six.', function() {
         const ocrProcessor = new Ocr();
 
-        const six = 
-        ' _ '+
-        '|_ '+
-        '|_|';
-
-        expect(ocrProcessor.processChar(six)).to.equal(6);
+        expect(ocrProcessor.mapEntryCharToValue(sixText)).to.equal(6);
     });
 
     it('processChar() should return 7 if given a text seven.', function() {
         const ocrProcessor = new Ocr();
 
-        const seven = 
-        ' _ '+
-        '  |'+
-        '  |';
-
-        expect(ocrProcessor.processChar(seven)).to.equal(7);
+        expect(ocrProcessor.mapEntryCharToValue(sevenText)).to.equal(7);
     });
 
     it('processChar() should return 8 if given a text eight.', function() {
         const ocrProcessor = new Ocr();
 
-        const eight = 
-        ' _ '+
-        '|_|'+
-        '|_|';
-
-        expect(ocrProcessor.processChar(eight)).to.equal(8);
+        expect(ocrProcessor.mapEntryCharToValue(eightText)).to.equal(8);
     });
 
     it('processChar() should return 9 if given a text nine.', function() {
         const ocrProcessor = new Ocr();
 
-        const nine = 
-        ' _ '+
-        '|_|'+
-        '  |';
-
-        expect(ocrProcessor.processChar(nine)).to.equal(9);
+        expect(ocrProcessor.mapEntryCharToValue(nineText)).to.equal(9);
     });
 
     it('processChar() should throw an ILL error if given an unrecognizable char.', function() {
@@ -122,6 +123,57 @@ describe('OCR', function() {
         '|||'+
         '|||';
 
-        expect(() => ocrProcessor.processChar(garbageChar)).to.throw('ILL');
-    });    
+        expect(() => ocrProcessor.mapEntryCharToValue(garbageChar)).to.throw('ILL');
+    });
+    
+    it('extractIndividualNumbers() should take a string entry and return an array of numberChar strings.', function() {
+        const ocrProcessor = new Ocr();
+        
+        const entryLine1 = '   ' + ' _ ' + ' _ ' + '   ' + ' _ ' + ' _ ' + ' _ ' + ' _ ' + ' _ ';
+        const entryLine2 = ' | ' + ' _|' + ' _|' + '|_|' + '|_ ' + '|_ ' + '  |' + '|_|' + '|_|';
+        const entryLine3 = ' | ' + '|_ ' + ' _|' + '  |' + ' _|' + '|_|' + '  |' + '|_|' + '  |';
+
+        const threeLineEntry = entryLine1 + entryLine2 + entryLine3;
+        const returnValue = ocrProcessor.extractIndividualNumbers(threeLineEntry);
+
+        expect(returnValue).to.be.an('array');
+        expect(typeof returnValue[0]).to.equal('string');
+        expect(returnValue.length).to.equal(9);
+        expect(returnValue[0].length).to.equal(9);
+
+        // Entry string contains only | _ and <space> characters.
+        returnValue.forEach((extractedChar) => {
+            expect(/^[_| ]*$/.test(extractedChar)).to.be.true;
+        });
+
+        expect(returnValue[0]).to.be.equal(oneText);
+        expect(returnValue[1]).to.be.equal(twoText);
+        expect(returnValue[2]).to.be.equal(threeText);
+        expect(returnValue[3]).to.be.equal(fourText);
+        expect(returnValue[4]).to.be.equal(fiveText);
+        expect(returnValue[5]).to.be.equal(sixText);
+        expect(returnValue[6]).to.be.equal(sevenText);
+        expect(returnValue[7]).to.be.equal(eightText);
+        expect(returnValue[8]).to.be.equal(nineText);       
+    });
+
+    it('processEntry() should return the numeric representation of the entry text.', function() {
+        const ocrProcessor = new Ocr();
+        
+        const entryLine1 = '   ' + '   ' + '   ' + '   ' + '   ' + '   ' + '   ' + '   ' + '   '
+        const entryLine2 = ' | ' + ' | ' + ' | ' + ' | ' + ' | ' + ' | ' + ' | ' + ' | ' + ' | ';
+        const entryLine3 = ' | ' + ' | ' + ' | ' + ' | ' + ' | ' + ' | ' + ' | ' + ' | ' + ' | ';
+
+        const threeLineEntry = entryLine1 + entryLine2 + entryLine3;
+        const returnValue = ocrProcessor.processEntry(threeLineEntry);
+    
+        expect(returnValue).to.be.a('number');
+        expect(returnValue.toString().length).to.equal(9);
+        expect(returnValue).to.equal(111111111);
+    });
+
+    it('convertTextNumbersToEntryValue() should take an array of text numbers and return a number.', function() {
+        const ocrProcessor = new Ocr();
+        expect(ocrProcessor.convertTextNumbersToEntryValue([oneText, twoText, threeText, fourText, fiveText, sixText, sevenText, eightText, nineText])).to.equal(123456789);
+    });
 });
