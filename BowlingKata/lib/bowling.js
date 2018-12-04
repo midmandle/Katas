3cb3cf72a0eeaf02d69c7cfdb729a858acabc7c5
Alasdair Smith <midmandle@users.noreply.github.com>
2018-12-04 18:45:56 +0700

    Updated isBonusFrame check to be correct
    isBonusFrame was previously incorrect, causing the all strike test case double count bonus rolls. It has been corrected to remove this.

--- BowlingKata/lib/bowling.js
+++ BowlingKata/lib/bowling.js
@@ -1,46 +1,78 @@
 class Bowling {
 	static calculateScore(gameScore) {
-		const rolls = gameScore.split('');
-		return rolls.reduce((acc, curr, index) => {
-			if(index > 9) {
-				return acc + 0;
-			}
-			if(curr === 'X') {
-				const strikeRolls = this.getStrikeRolls(rolls, index);
-				return acc + this.getValueOfRolls(strikeRolls);
-			} else if (curr === '/') {
-				const spareRolls = this.getSpareRolls(rolls, index);
-				return acc + this.getValueOfRolls(spareRolls);
-			} else {
-				return acc + this.getValueOfRoll(curr);
-			}
+		const frames = gameScore.split(' ');
+		return frames.reduce((acc, curr, index) => {
+			return acc + this.getFrameScore(frames, index);
 		}, 0);
 	}
 
-	static getStrikeRolls(rolls, index) {
-		return rolls.slice(index, index + 3);
-	}
+	static getFrameScore(frames, index) {
+		const currentFrame = frames[index];
 
-	static getSpareRolls(rolls, index) {
-		return rolls.slice(index, index + 2);
-	}
-
-	static getValueOfRolls(rolls) {
-		return rolls.reduce((acc, curr) => {
-			return acc + this.getValueOfRoll(curr);
-		}, 0);
-	}
-
-	static getValueOfRoll(roll) {
-		if(roll === 'X' || roll === '/') {
-			return 10;
-		} else if (roll !== undefined) {
-			return parseInt(roll);
-		} else {
+		if (this.isBonusFrame(frames, index)) {
 			return 0;
+		}
+
+		if(currentFrame.includes('X') || currentFrame.includes('/')) {
+			return 10 + this.getBonusScore(frames, index);
+		}
+
+		return this.getFrameValue(currentFrame);
+	}
+
+	static isBonusFrame(frames, index) { 
+		console.log('index :', index);
+		if (index > 9 && this.isFullGame(frames)) {
+			console.log('frames :', frames);
+			console.log('index :', index);
+			return true;
+		}
+	}
+
+	static isLastFrame(frames, index) {
+		return index === (frames.length - 1);
+	}
+
+	static isFullGame(frames) {
+		return frames.length >= 10;
+	}
+
+	static getBonusScore(frames, index) {
+		const currentFrame = frames[index];
+		const upcomingRolls = frames.slice(index+1).join('');
+		if (currentFrame.includes('X')) {
+			// Return the next 2 rolls.
+			return this.getRollValue(upcomingRolls[0]) + this.getRollValue(upcomingRolls[1]);
+		} else if (currentFrame.includes('/') ) {
+			// Return the next 1 roll.
+			return this.getRollValue(upcomingRolls[0]);
+		}
+	}
+
+	static getFrameValue(frame) {
+		if (frame === undefined) {
+			return 0;
+		}
+
+		const rolls = frame.split('');
+		return rolls.reduce((acc, curr) => {
+			return acc + this.getRollValue(curr);
+		}, 0);
+	}
+
+	static getRollValue(roll) {
+		if (roll === undefined) {
+			return 0;
+		}
+
+		if (roll === '-') {
+			return 0;
+		} else if ( roll === 'X' || roll === '/') {
+			return 10;
+		} else {
+			return parseInt(roll);
 		}
 	}
 }
 
 module.exports = Bowling;
-
--- BowlingKata/test/bowling.test.js
+++ BowlingKata/test/bowling.test.js
@@ -9,32 +9,44 @@ describe('Bowling', () => {
 	});
 
 	it('it should calculate the score for multiple sets', () => {
-		const gameRecord = '1111';
+		const gameRecord = '11 11';
 		const gameScore = Bowling.calculateScore(gameRecord);
 		expect(gameScore).eql(4);
 	});
 
 	it('should interpret strikes', () => {
-		const gameRecord = 'X11';
+		const gameRecord = 'X 11';
 		const gameScore = Bowling.calculateScore(gameRecord);
 		expect(gameScore).eql(14);
 	});
 
 	it('should interpret spares', () => {
-		const gameRecord = '/1';
+		const gameRecord = '1/ 1-';
 		const gameScore = Bowling.calculateScore(gameRecord);
 		expect(gameScore).eql(12);
 	});
 
 	it('should calculate scores with strikes to include the next 2 rolls from the following set', () => {
-		const gameRecord = 'X1111';
+		const gameRecord = 'X 11 11';
 		const gameScore = Bowling.calculateScore(gameRecord);
 		expect(gameScore).eql(16);
 	});
 
-	it('should calculate a gameRecord of: XXXXXXXXXXXX as 300', () => {
-		const gameRecord = 'XXXXXXXXXXXX';
+	it('should calculate a gameRecord of: X X X X X X X X X X X X as 300', () => {
+		const gameRecord = 'X X X X X X X X X X X X';
 		const gameScore = Bowling.calculateScore(gameRecord);
 		expect(gameScore).eql(300);
+	});
+
+	it('should interpret \'-\' as a miss', () => {
+		const gameRecord = '9- 9- 9- 9- 9- 9- 9- 9- 9- 9-';
+		const gameScore = Bowling.calculateScore(gameRecord);
+		expect(gameScore).eql(90);		
+	});
+
+	it('should interpret the space betweek the game records as a frame delimiter', () => {
+		const gameRecord = '5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5';
+		const gameScore = Bowling.calculateScore(gameRecord);
+		expect(gameScore).eql(150);
 	});
 });
